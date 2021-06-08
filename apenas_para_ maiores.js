18b4fe644edb2bbc84528592a528af4279a8a5f9
Fábio <fabiogrp@gmail.com>
2021-06-08 20:24:28 -0300

    Esse foi fácil

---
+++ apenas_para_ maiores.js
@@ -0,0 +1,11 @@
+function solucao(idade) {
+    if(idade >=18)
+      { console.log('PODE ENTRAR');
+       
+       }else{console.log('ACESSO NEGADO');}
+    
+  }
+  
+  function processData(input) {
+        solucao(parseInt(input.trim(), 10));
+  } 
